84f918f5c9981be7767a40c0501c42d86106e15d
Prince Goziem <princegoziem@gmail.com>
2018-12-01 04:57:47 +0100

    chore(setup-react-project): setup empty react project
    - Installed all necessary package
    - Configured Webpack
    - Configured Babel
    - Setup scripts in package.json file
    
    [Delivers #162345973]

---
+++ src/components/Router.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AppRouter = () => {
+  return (
+    <div>
+      <h1>React Setup</h1>
+    </div>
+  )
+}
+
+export default AppRouter;
---
+++ webpack.config.dev.js
@@ -0,0 +1,23 @@
+// third-party libraries
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+// webpack common configuration
+const common = require('./webpack.config.common.js');
+
+/**
+ * @desc development configuration
+ */
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development'),
+        CLIENT_REDIRECT: JSON.stringify('http://localhost:8080')
+      }
+    })
+  ]
+});
